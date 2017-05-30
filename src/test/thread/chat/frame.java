bb7d5299e884726f46c9c4e5ee1715592573b4de
chozer <1521756898@qq.com>
2017-05-30 20:39:40 +0800

    1.3

--- src/test/thread/chat/frame.java
+++ src/test/thread/chat/frame.java
@@ -8,45 +8,70 @@ import java.net.Socket;
 public class frame extends Frame implements ActionListener{
 	static TextArea text;
 	static TextArea rece;
-	//PrintWriter out;
-	Button send = new Button("send");
+	Button send;
+	Button sign;
+	TextField tname;
+	Panel sname;
 	static String s = "";
-	//Send sen;
+	static String name = "x";
 	DataOutputStream out ;
+	/*public void sframe(){
+		
+		setTitle("Login");
+		setLayout(new GridLayout(1,3));
+		Label name = new Label("name");
+		sign = new Button("sign");
+		tname = new TextField();
+		this.addWindowListener(new WinCilent(this));
+		sign.addActionListener(this);
+		add(name);
+		add(tname);
+		add(sign);
+		setVisible(true);
+	}*/
 	public frame(Socket cilent) throws IOException{
-		setTitle("cilent");
+		setTitle("Cilent");
 		setLayout(new BorderLayout());
+		sname = new Panel();
+		sname.setLayout(new GridLayout(2,1));
+		send = new Button("send");
+		sign = new Button("change");
 		this.addWindowListener(new WinCilent(this));
 		send.addActionListener(this);
+		sign.addActionListener(this);
 		text = new TextArea(40,60);
 		rece = new TextArea(10,50);
+		tname = new TextField();
 		send.setSize(WIDTH, 30);
-		add(send,BorderLayout.SOUTH);
-		add(text,BorderLayout.CENTER);
+		sname.add(tname,BorderLayout.NORTH);
+		sname.add(sign, BorderLayout.SOUTH);
+		add(sname,BorderLayout.WEST);
 		add(rece,BorderLayout.NORTH);
+		add(text,BorderLayout.CENTER);
+		add(send,BorderLayout.SOUTH);
 		setVisible(true);
-		//new Thread(new Send(cilent)).start();
-		//sen = new Send();
 		out = new DataOutputStream(cilent.getOutputStream());
 		new Thread(new Receive(cilent)).start();
 	}
-	/*public void mousePressde(MouseEvent e){
-		
-	}
-	public void mouseReleased(MouseEvent e){
-		text.setText("");
-	}*/
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		s = text.getText();
-		//sen.send1();
+		if(e.getSource().equals(sign)){
+			name = tname.getText();
+			System.out.println(name);
+		}
+		if(e.getSource().equals(send)){
+			
+			s = text.getText();
+			rece.append(name+"-"+s+"\n");
 		try {
-			out.writeUTF(s);
+			out.writeUTF(name+"-"+s);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		}
+		
 	}
 	public  static String get(){
 		return s;
